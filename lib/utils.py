7a8b4227b7d14365d4743f583218c696cf93ba74
sameepv21 <sameep0112vani@gmail.com>
2024-04-05 11:12:32 -0700

    feat: completed training code

--- lib/utils.py
+++ lib/utils.py
@@ -5,6 +5,15 @@ import torch.nn as nn
 # Data Manipulation Lib
 import numpy as np
 
+def get_batch_single(data, t, batch_len=64, batch_size=64, device = torch.device("cpu")):
+	s = torch.from_numpy(np.random.choice(np.arange(len(t) - batch_len, len(t), dtype=np.int64), batch_size, replace=False))
+
+	batch_y0 = data[0,s]  # (M, D)
+	batch_t = t[:batch_len]  # (T)
+	batch_y = torch.stack([data[0,s]], dim=1)  # (T, M, D)
+	
+	return batch_y0.to(device), batch_t.to(device), batch_y.to(device)
+
 def create_net(n_inputs, n_outputs, n_layers = 1, 
 	n_units = 100, nonlinear = nn.Tanh):
 	if n_layers == 0:
--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torchdiffeq import odeint_adjoint as odeint
 
 # Data Manipulation Lib
 import numpy as np
@@ -10,19 +11,26 @@ import random
 # Data Loading Lib
 import json
 
+# Helper Modules
+from lib.utils import *
+from lib.odefunc import *
+
 # Misc
 import os
 import warnings
 warnings.filterwarnings('ignore')
 
 # Define some constants
-ROOT_PATH = "/Neural_ODE_x_SR/data"
+ROOT_PATH = "./data"
 FILE_NAME = 'strogatz_extended.json'
 SEED = 42
+EPOCHS = 1000
+N_LAYERS = 4
+N_UNITS = 50
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Define some default settings
 torch.set_default_dtype(torch.float64)
-decide = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(SEED)
 random.seed(SEED)
 np.random.seed(SEED)
@@ -31,4 +39,60 @@ np.random.seed(SEED)
 with open(os.path.join(ROOT_PATH, FILE_NAME)) as strogatz:
     data = np.array(json.load(strogatz))
 
-X_train = torch.tensor()
+# Define Neural Networks for each of the ODE and each initial condition in the dataset
+ode_funcs = []
+for ode in data:
+    for dim in range(len(ode['init'])):
+        ode_func = ODEfunc(ode['dim'], N_LAYERS, N_UNITS, DEVICE)
+        ode_funcs.append(ode_func)
+
+# Set best loss for each ode to be -ve infinity
+best_loss = [-float('inf')] * len(ode_funcs)
+
+# Make sure that there is no discrepancy
+assert len(best_loss) == len(ode_funcs)
+
+# Loop to train a single ode
+from lib.utils import *
+
+for index, neural_ode in enumerate(ode_funcs):
+    id = index + 1 # id of the ode under consideration
+
+    # Get the training data
+    X_train_dict = data[index]['solutions'][0]
+
+    # Number of trajectories 
+    num_traj = int(data[index]['dim'])
+
+    # Train for each trajectory
+    for i in range(num_traj):
+        # Define optimizer and scheduler
+        optimizer = optim.Adamax(neural_ode.parameters())
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.9987)
+        
+        # Training loop
+        for itr in range(EPOCHS):
+
+            # Separate required variables
+            X_train = torch.tensor(X_train_dict[i]['y'])
+            t = torch.tensor(X_train_dict[i]['t'])
+            init_values = X_train_dict[i]['init']
+
+            # Set required variables
+            optimizer.zero_grad()
+            
+            # Get batches of data
+            batch_y0, batch_t, batch_y = get_batch_single(X_train, t, device = DEVICE)
+            batch_y0 = batch_y0.reshape(-1, 1)
+
+            # Train the Neural ODEs
+            pred_y = odeint(neural_ode, batch_y0, batch_t)
+            loss = torch.mean((pred_y - batch_y)**2) * 10e4
+            
+            # Print the loss
+            print(f'Epoch {itr} / {EPOCHS} Loss {loss.item()}')
+            
+            # Backprop and step up
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
